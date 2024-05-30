38faa628e6e1af3cf5cb188a24a4c7eee4275d35
ferutizi <fer.utizi@gmail.com>
2024-05-30 20:10:56 -0300

    add: getExpensesRequest and map data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,19 @@
+import { getExpensesRequest } from "./api/expenses";
 
-export default function Home() {
+export default async function Home() {
+  const expenses = await getExpensesRequest()
+  console.log(expenses)
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
+      <h1>Expenses</h1>
+      {expenses.map(e => 
+        <div key={e.id}>
+          <h2>{e.detail}</h2>
+          <p>{e.date.toString()}</p>
+          <p>{e.section}</p>
+          <p>${e.value}</p>
+        </div>
+      )}
     </main>
   );
 }
