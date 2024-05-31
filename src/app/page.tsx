34a29b66697c12e4f6f19e2a62df964ec59d7990
ferutizi <fer.utizi@gmail.com>
2024-05-31 20:19:48 -0300

    add: limit max width to 900px and add sectionButton component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,25 @@
 import { getExpenses } from "./api/expenses";
 import CreateNewExpense from "./components/CreateNewExpense";
+import ExpenseCard from "./components/ExpenseCard";
+import SectionButton from "./components/SectionButton";
 
 export default async function Home() {
   const expenses = await getExpenses()
   console.log(expenses)
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen max-w-[900px] items-center flex-col justify-between m-auto p-16">
       <CreateNewExpense></CreateNewExpense>
-      <h1>Expenses</h1>
+      <h1>Gastos</h1>
+      <div className="flex gap-2">
+        <SectionButton section="Comida" />
+        <SectionButton section="Ropa" />
+        <SectionButton section="Transporte" />
+        <SectionButton section="Impuestos" />
+        <SectionButton section="Salud" />
+        <SectionButton section="Ocio" />
+      </div>
       {expenses.map(e => 
-        <div key={e.id}>
-          <h2>{e.detail}</h2>
-          <p>{e.date.toString()}</p>
-          <p>{e.section}</p>
-          <p>${e.value}</p>
-        </div>
+        <ExpenseCard key={e.id} expense={e} />
       )}
     </main>
   );
