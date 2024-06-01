433dfe5f96d7df775172b79f757051137c30128c
ferutizi <fer.utizi@gmail.com>
2024-05-31 21:18:40 -0300

    add: mapping styles in expense card

--- src/app/components/ExpenseCard.tsx
+++ src/app/components/ExpenseCard.tsx
@@ -1,21 +1,29 @@
-import { Expense } from "../../../types"
+import { Expense, Section } from "../../../types"
 
 interface ExpenseCardProps {
   expense: Expense
 }
 
+const sections: { [key in Section]: string } = {
+  "comida": 'border-t-green-500',
+  "ropa": 'border-t-amber-500',
+  "transporte": 'border-t-violet-500',
+  "impuestos": 'border-t-red-500',
+  "salud": 'border-t-cyan-500',
+  "ocio": 'border-t-pink-500'
+}
+
 export default function ExpenseCard({expense}: ExpenseCardProps) {
+  const section = expense.section.toLowerCase() as Section
+  const sectionClass = sections[section]
+
   return(
-    <article className="w-80 bg-zinc-950 h-32">
-      <div className="flex">
-        <h2>{expense.detail}</h2>
-        <p>{expense.value}</p>
+    <article className={`flex flex-col justify-between w-[26rem] ${sectionClass} p-4 border border-t-[3px] rounded-lg h-32`}>
+      <div className="flex justify-between">
+        <h2 className="break-words w-[70%]">{expense.detail}</h2>
+        <p>$ {expense.value}</p>
       </div>
-{/*       <div className="flex justify-center bg-red-500 w-fit min-w-28 rounded-lg">
-        {expense.section}
-      </div> */}
-      <p className="text-red-500">{expense.section}</p>
-      <p>{expense.date.toString()}</p>
+      <p className="opacity-60">{expense.date.toString()}</p>
     </article>
   )
 }
