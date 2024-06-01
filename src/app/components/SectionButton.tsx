e1f036f09bef3f7993a4c90d955ee1cde75e5990
ferutizi <fer.utizi@gmail.com>
2024-06-01 18:40:47 -0300

    add: todo section for show all sections

--- src/app/components/SectionButton.tsx
+++ src/app/components/SectionButton.tsx
@@ -7,6 +7,7 @@ interface SectionButtonProps {
 }
 
 const sections: { [key in Section]: string[] } = {
+  "todo": ['hover:text-stone-900', 'border-t-stone-950'],
   "comida": ['hover:text-green-700', 'border-t-green-500'],
   "ropa": ['hover:text-amber-700', 'border-t-amber-500'],
   "transporte": ['hover:text-violet-700', 'border-t-violet-500'],
@@ -19,7 +20,7 @@ export default function SectionButton({section}: SectionButtonProps) {
   const sectionClass = sections[section] || ''
 
   return(
-    <button className={`border rounded-lg px-1 min-w-24 h-8 ${sectionClass[0]} ${sectionClass[1]} font-bold border-t-[3px]`}>
+    <button className={`border rounded-lg px-1 min-w-24 h-8 ${sectionClass[0]} ${sectionClass[1]} hover:font-bold border-t-[3px]`}>
       {section}
     </button>
   )
