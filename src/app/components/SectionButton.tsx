dd427ae79cba6d8128acc6703ece1c6ccd46113e
ferutizi <fer.utizi@gmail.com>
2024-05-31 21:18:22 -0300

    add: mapping styles sections card

--- src/app/components/SectionButton.tsx
+++ src/app/components/SectionButton.tsx
@@ -6,20 +6,20 @@ interface SectionButtonProps {
   section: Section;
 }
 
-const sections: { [key in Section]: string } = {
-  "Comida": 'text-green-700 border-t-green-500',
-  "Ropa": 'text-amber-700 border-t-amber-500',
-  "Transporte": 'text-violet-700 border-t-violet-500',
-  "Impuestos": 'text-red-700 border-t-red-500',
-  "Salud": 'text-cyan-700 border-t-cyan-500',
-  "Ocio": 'text-pink-700 border-t-pink-500',
+const sections: { [key in Section]: string[] } = {
+  "comida": ['hover:text-green-700', 'border-t-green-500'],
+  "ropa": ['hover:text-amber-700', 'border-t-amber-500'],
+  "transporte": ['hover:text-violet-700', 'border-t-violet-500'],
+  "impuestos": ['hover:text-red-700', 'border-t-red-500'],
+  "salud": ['hover:text-cyan-700', 'border-t-cyan-500'],
+  "ocio": ['hover:text-pink-700', 'border-t-pink-500'],
 }
 
 export default function SectionButton({section}: SectionButtonProps) {
   const sectionClass = sections[section] || ''
 
   return(
-    <button className={`border rounded-lg px-1 min-w-24 h-8 ${sectionClass} font-bold border-t-[3px]`}>
+    <button className={`border rounded-lg px-1 min-w-24 h-8 ${sectionClass[0]} ${sectionClass[1]} font-bold border-t-[3px]`}>
       {section}
     </button>
   )
