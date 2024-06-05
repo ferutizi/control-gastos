2fd2f02d0adbe3f9c1b5a722e99897a143c2374b
ferutizi <fer.utizi@gmail.com>
2024-06-05 17:01:51 -0300

    add: button dynamic styles with sectionParam

--- src/app/components/SectionButton.tsx
+++ src/app/components/SectionButton.tsx
@@ -5,19 +5,20 @@ import { usePathname, useSearchParams, useRouter } from "next/navigation";
 
 interface SectionButtonProps {
   section: Section;
+  sectionParam: string | undefined
 }
 
 const sections: { [key in Section]: string[] } = {
-  "todo": ['hover:text-stone-900', 'border-t-stone-950'],
-  "comida": ['hover:text-green-700', 'border-t-green-500'],
-  "ropa": ['hover:text-amber-700', 'border-t-amber-500'],
-  "transporte": ['hover:text-violet-700', 'border-t-violet-500'],
-  "impuestos": ['hover:text-red-700', 'border-t-red-500'],
-  "salud": ['hover:text-cyan-700', 'border-t-cyan-500'],
-  "ocio": ['hover:text-pink-700', 'border-t-pink-500'],
+  "todo": ['hover:text-stone-900', 'border-t-stone-950', 'text-stone-900 font-bold'],
+  "comida": ['hover:text-green-700', 'border-t-green-500', 'text-green-700 font-bold'],
+  "ropa": ['hover:text-amber-700', 'border-t-amber-500', 'text-amber-700 font-bold'],
+  "transporte": ['hover:text-violet-700', 'border-t-violet-500', 'text-violet-700 font-bold'],
+  "impuestos": ['hover:text-red-700', 'border-t-red-500', 'text-red-700 font-bold'],
+  "salud": ['hover:text-cyan-700', 'border-t-cyan-500', 'text-cyan-700 font-bold'],
+  "ocio": ['hover:text-pink-700', 'border-t-pink-500', 'text-pink-700 font-bold'],
 }
 
-export default function SectionButton({section}: SectionButtonProps) {
+export default function SectionButton({section, sectionParam}: SectionButtonProps) {
 
   const searchParams = useSearchParams()
   const pathName = usePathname()
@@ -34,10 +35,15 @@ export default function SectionButton({section}: SectionButtonProps) {
     replace(`${pathName}?${params.toString()}`)
   }
 
+  const buttonStyle = 'border rounded-lg px-1 min-w-24 h-8 hover:font-bold border-t-[3px]'
+  const isActive = section == sectionParam
   const sectionClass = sections[section] || ''
+  const isAll = !sectionParam
 
   return(
-    <button onClick={() => handleSearchSection(section)} className={`border rounded-lg px-1 min-w-24 h-8 ${sectionClass[0]} ${sectionClass[1]} hover:font-bold border-t-[3px]`}>
+    <button
+      onClick={() => handleSearchSection(section)}
+      className={`${buttonStyle} ${sectionClass[0]} ${sectionClass[1]} ${isActive && sectionClass[2]} ${isAll && section === 'todo' && 'font-bold'} `}>
       {section}
     </button>
   )
