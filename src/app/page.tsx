b87677cf3df5c981833dbace1056d1f1c0567693
ferutizi <fer.utizi@gmail.com>
2024-06-05 17:00:39 -0300

    add: sectionParam pass by param to SectionButton and createNewExpense components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,15 +16,15 @@ export default async function Home({
   return (
     <main className="flex min-h-screen max-w-[900px] items-center flex-col justify-start gap-10 m-auto p-6 lg:p-16">
       <div className="flex gap-2 flex-wrap justify-center">
-        <SectionButton section="todo" />
-        <SectionButton section="comida" />
-        <SectionButton section="ropa" />
-        <SectionButton section="transporte" />
-        <SectionButton section="impuestos" />
-        <SectionButton section="salud" />
-        <SectionButton section="ocio" />
+        <SectionButton sectionParam={sectionParam} section="todo" />
+        <SectionButton sectionParam={sectionParam} section="comida" />
+        <SectionButton sectionParam={sectionParam} section="ropa" />
+        <SectionButton sectionParam={sectionParam} section="transporte" />
+        <SectionButton sectionParam={sectionParam} section="impuestos" />
+        <SectionButton sectionParam={sectionParam} section="salud" />
+        <SectionButton sectionParam={sectionParam} section="ocio" />
       </div>
-      <CreateNewExpense />
+      <CreateNewExpense sectionParam={sectionParam} />
       <section className="flex flex-col-reverse gap-8">
         {expenses
         .filter(expense => !sectionParam || expense.section === sectionParam)
