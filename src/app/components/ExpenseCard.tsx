94268a17f7e7b50bc796e5b235b3c666f64d092f
ferutizi <fer.utizi@gmail.com>
2024-06-04 22:48:37 -0300

    refactor: convert editButton to Dynamic

--- src/app/components/ExpenseCard.tsx
+++ src/app/components/ExpenseCard.tsx
@@ -1,6 +1,8 @@
+import dynamic from "next/dynamic"
 import { Expense, Section } from "../../../types"
-import IconEdit from "./IconEdit"
 import IconTrash from "./IconTrash"
+import { useState } from "react"
+import EditButton from "./EditButton"
 
 interface ExpenseCardProps {
   expense: Expense
@@ -20,6 +22,14 @@ export default function ExpenseCard({expense}: ExpenseCardProps) {
   const section = expense.section.toLowerCase() as Section
   const sectionClass = sections[section]
 
+  function editButton() {
+    return(
+      <EditButton expense={expense} />
+    )
+  }
+
+  const DynamicEditButton =  dynamic(async () => editButton, {ssr: false}) 
+
   return(
     <article className={`flex flex-col justify-between w-[26rem] ${sectionClass} p-4 border border-t-[3px] rounded-lg h-32`}>
       <div className="flex justify-between">
@@ -29,8 +39,8 @@ export default function ExpenseCard({expense}: ExpenseCardProps) {
       <div className="flex justify-between">
         <p className="text-gray-500">{expense.date.toString()}</p>
         <div className="flex gap-4">
-          <IconEdit />
-          <IconTrash id={expense.id} />
+        <DynamicEditButton />
+        <IconTrash id={expense.id} />
         </div>
       </div>
     </article>
