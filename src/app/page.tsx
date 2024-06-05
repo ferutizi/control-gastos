972b2d6aa66e75b0e2243be47c7a0a8193c793f4
ferutizi <fer.utizi@gmail.com>
2024-06-05 00:19:19 -0300

    add: use Search Params to filter expenses before map

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,19 @@ import CreateNewExpense from "./components/CreateNewExpense";
 import ExpenseCard from "./components/ExpenseCard";
 import SectionButton from "./components/SectionButton";
 
-export default async function Home() {
+export default async function Home({
+  searchParams
+}: {
+  searchParams?: {
+    section?: string
+  }
+}) {
+  const sectionParam = searchParams?.section 
   const expenses = await getExpenses()
 
   return (
-    <main className="flex min-h-screen max-w-[900px] items-center flex-col justify-center gap-10 m-auto p-16">
-      <div className="flex gap-2">
+    <main className="flex min-h-screen max-w-[900px] items-center flex-col justify-start gap-10 m-auto p-6 lg:p-16">
+      <div className="flex gap-2 flex-wrap justify-center">
         <SectionButton section="todo" />
         <SectionButton section="comida" />
         <SectionButton section="ropa" />
@@ -18,8 +25,10 @@ export default async function Home() {
         <SectionButton section="ocio" />
       </div>
       <CreateNewExpense />
-      <section className="flex flex-col gap-8">
-        {expenses.map(e => 
+      <section className="flex flex-col-reverse gap-8">
+        {expenses
+        .filter(expense => !sectionParam || expense.section === sectionParam)
+        .map(e => 
           <ExpenseCard key={e.id} expense={e} />
         )}
       </section>
